fef979c9bd785f86b1ba4feaae25aaa9c5887663
anusimalti1997 <anupamsimalti1997@gmail.com>
2022-11-07 08:40:05 +0530

    added download resume functionality

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,7 @@
 // Scripts
 // 
 
+
 window.addEventListener('DOMContentLoaded', event => {
 
     // Activate Bootstrap scrollspy on the main nav element
@@ -34,5 +35,36 @@ window.addEventListener('DOMContentLoaded', event => {
 });
 document.getElementById('copytext').addEventListener('click',function(){
     navigator.clipboard.writeText('9910763513')
-   
+
 })
+
+document.getElementById('download').addEventListener('input',function(e){
+    debugger
+    var reader = new FileReader();
+    var fileByteArray = [];
+    let myFile = e.target.files[0];
+    reader.readAsArrayBuffer(myFile);
+    reader.onloadend = function (evt) {
+    if (evt.target.readyState == FileReader.DONE) {
+       var arrayBuffer = evt.target.result,
+           array = new Uint8Array(arrayBuffer);
+       for (var i = 0; i < array.length; i++) {
+           fileByteArray.push(array[i]);
+        }
+    }
+
+
+}
+let result = "";
+for (var i = 0; i < fileByteArray.length; i++) {
+  result += String.fromCharCode(parseInt(array[i], 2));
+}
+
+
+console.log(result)
+console.log(fileByteArray)
+})
+
+
+
+
